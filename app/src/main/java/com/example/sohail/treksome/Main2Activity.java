dc3880d407df2d77f695377de45eaa124f9fa4a8
kabirSohail <kabeersohel@gmail.com>
2018-05-18 01:52:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sohail/treksome/Main2Activity.java
@@ -0,0 +1,127 @@
+package com.example.sohail.treksome;
+
+import android.app.ProgressDialog;
+import android.app.VoiceInteractor;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.Arrays;
+import java.util.UUID;
+
+import br.com.bloder.magic.view.MagicButton;
+
+public class Main2Activity extends AppCompatActivity {
+    MagicButton magicGoogleButton;
+    Button JavaUploadButton,JavaChooseButton;
+    FirebaseStorage storage;
+    StorageReference storageReference;
+    ImageView imageView;
+    private static final int Pick_Image_Request = 91;
+    private Uri filePath;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+        Toast.makeText(Main2Activity.this,"Sign in success",Toast.LENGTH_SHORT).show();
+            magicGoogleButton = findViewById(R.id.XmlGoogleSignOut);
+            JavaChooseButton = findViewById(R.id.XmlChooseImage);
+            JavaUploadButton = findViewById(R.id.XmlUploadImage);
+            imageView = findViewById(R.id.XmlImage);
+            JavaUploadButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    uploadImage();
+                }
+            });
+
+            JavaChooseButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    chooseImage();
+                }
+            });
+
+            magicGoogleButton.setMagicButtonClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AuthUI.getInstance().signOut(Main2Activity.this);
+                startActivity(new Intent(Main2Activity.this,MainActivity.class));
+            }
+        });
+
+
+
+    }
+
+    private void uploadImage() {
+        if(filePath != null){
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading");
+            progressDialog.show();
+            StorageReference reference = storageReference.child("images/"+ UUID.randomUUID().toString());
+            reference.putFile(filePath).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    progressDialog.dismiss();
+                    Toast.makeText(Main2Activity.this,"Uploade Successful ",Toast.LENGTH_SHORT).show();
+                }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(Main2Activity.this,"Uploade failed "+e.getMessage(),Toast.LENGTH_SHORT).show();
+                }
+            })
+            .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                    double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount());
+                    progressDialog.setMessage("Uploade status "+(int)progress+"%");
+                }
+            });
+        }
+    }
+
+    private void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent.createChooser(intent,"Choose image"),Pick_Image_Request);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == Pick_Image_Request && resultCode == RESULT_OK && data != null && data.getData() != null){
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),filePath);
+                imageView.setImageBitmap(bitmap);
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/sohail/treksome/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.sohail.treksome;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.sohail.treksome.Main2Activity;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.common.SignInButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+
+import br.com.bloder.magic.view.MagicButton;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 1;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+    MagicButton magicGoogleButton;
+    FirebaseAuth.AuthStateListener stateListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+
+
+        magicGoogleButton = findViewById(R.id.XmlGoogleSignIn);
+        magicGoogleButton.setMagicButtonClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(
+                        AuthUI.getInstance()
+                                .createSignInIntentBuilder()
+                                .setAvailableProviders(Arrays.asList(
+                                        new AuthUI.IdpConfig.GoogleBuilder().build()
+                                ))
+                                .build(),
+                        RC_SIGN_IN);
+            }
+        });
+        stateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                firebaseUser = firebaseAuth.getCurrentUser();
+                if(firebaseUser == null)
+                {
+                    Toast.makeText(MainActivity.this,"Signed out",Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this,"Signed in",Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this,Main2Activity.class));
+                }
+            }
+        };
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        firebaseAuth.addAuthStateListener(stateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        firebaseAuth.removeAuthStateListener(stateListener);
+    }
+}
